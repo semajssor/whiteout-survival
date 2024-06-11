64e372a906ea4d33f96daf1d43c1231d8a171fdf
semajssor <141041402+semajssor@users.noreply.github.com>
2024-06-11 15:13:00 +0300

    other attemp

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path'); // Node.js module for working with file paths
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path'; // Node.js module for working with file paths
+
+dotenv.config();
 
 const app = express();
 
-// Middleware for parsing JSON bodies
 app.use(express.json());
-
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+const PORT = process.env.PORT || 3000;
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
-});
+  });
 
-// Define Mongoose schema and model
 const memberSchema = new mongoose.Schema({
   rank: String,
   username: { type: String, unique: true },
@@ -27,27 +28,6 @@ const memberSchema = new mongoose.Schema({
   level: Number
 });
 const Member = mongoose.model('Member', memberSchema);
-
-// API routes
-function displayUsers() {
-  fetch('/members')
-    .then(response => response.json())
-    .then(data => {
-      const userList = document.getElementById('userList');
-      userList.innerHTML = '';
-      data.forEach(user => {
-        const userDiv = document.createElement('div');
-        userDiv.classList.add('user-item');
-        userDiv.innerHTML = `
-          <p>Rank: ${user.rank}</p>
-          <p>Username: ${user.username}</p>
-          <p>Power: ${user.power}</p>
-          <p>Level: ${user.level}</p>
-        `;
-        userList.appendChild(userDiv);
-      });
-    });
-}
 
 app.post('/members', async (req, res) => {
   try {
@@ -87,13 +67,10 @@ app.delete('/members/:username', async (req, res) => {
   }
 });
 
-// Serve 'index.html' for all other routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
